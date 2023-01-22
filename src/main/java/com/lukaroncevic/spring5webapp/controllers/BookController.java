6aeaa1fcf805175b41fe32745ab4fba35f19c9fc
“lroncevic” <roncevicluka@outlook.com>
2023-01-22 17:49:45 +0100

    Add List of Authors

--- src/main/java/com/lukaroncevic/spring5webapp/controllers/BookController.java
+++ src/main/java/com/lukaroncevic/spring5webapp/controllers/BookController.java
@@ -13,11 +13,14 @@ public class BookController {
     public BookController(BookRepository bookRepository){
         this.bookRepository = bookRepository;
     }
+
     @RequestMapping("/books")
-    public String getBooks(Model model){
+        public String getBooks(Model model){
 
         model.addAttribute("books", bookRepository.findAll());
 
         return "books/list";
+
     }
+
 }
